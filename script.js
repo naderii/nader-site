185cff95e7d14732404d64e5c8dae6e04c6ce858
nader <nader@kashan.ir>
2025-05-11 13:06:20 +0330

    add css

---
+++ script.js
@@ -0,0 +1,15 @@
+// تایپ متن
+var typed = new Typed('#typed', {
+    strings: ['نادر نادری', 'برنامه نویس شبکه', 'DevOps Engineer'],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true
+  });
+  
+  // انیمیشن اسکرول
+  AOS.init({
+    duration: 1000,
+    once: true
+  });
+  
